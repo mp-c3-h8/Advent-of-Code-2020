64bd75260cf4739a269be8583efda3822ebac0c7
mp <no@e.mail>
2026-01-29 05:49:59 +0100

    day20

--- day20/day20.py
+++ day20/day20.py
@@ -4,67 +4,53 @@ import numpy as np
 from typing import Iterator
 from collections import defaultdict, Counter
 from math import prod
+from itertools import product
+from scipy.ndimage import convolve
 
-type Orientation = tuple[int, int]  # (a,b), a: number of rot90 in {0,1,2,3}, b: fliplr yes/no in {0,1}
-type Pos = complex  # y = i downwards
+type Pos = complex  # y = 1j downwards
 type Dir = complex
 type Grid = dict[Pos, Tile | None]
+type Edge = np.ndarray
+type EdgeTuple = tuple[int, ...]
 
 DIRS: dict[str, Dir] = {"N": -1j, "E": 1, "S": 1j, "W": -1}
-
-
-class Image:
-    def __init__(self, tiles: list[Tile]) -> None:
-        self.tiles = tiles
-        self.dim = int(np.sqrt(len(tiles)))
-        assert self.dim*self.dim == len(tiles), f"No quadratic shape possible for {len(tiles)} tiles."
-
-    def __repr__(self) -> str:
-        return str(self.tiles)
 
 
 class Tile:
     def __init__(self, idx: int, matrix: np.ndarray) -> None:
         self.idx = idx
         self.matrix = matrix
-        self.orientation: Orientation = (-1, -1)
 
     # https://en.wikipedia.org/wiki/Dihedral_group_of_order_8
-    def symmetries(self) -> Iterator[tuple[np.ndarray, Orientation]]:
-        a = self.matrix
-        for k in range(4):
-            yield a, (k, 0)
-            yield np.fliplr(a), (k, 1)
-            a = np.rot90(a)
+    def transform(self) -> Iterator[None]:
+        for _ in range(2):
+            for _ in range(4):
+                yield None
+                self.matrix = np.rot90(self.matrix)
+            self.matrix = np.fliplr(self.matrix)
 
-    def north_and_west_edges(self) -> Iterator[tuple[list[np.ndarray], Orientation]]:
-        for a, o in self.symmetries():
-            yield [a[0, :], a[:, 0]], o
+    # get specific edge
+    def get_edge(self, d: Dir) -> Edge:
+        match d:
+            case -1j: return self.matrix[0, :]   # N
+            case   1: return self.matrix[:, -1]  # E
+            case  1j: return self.matrix[-1, :]  # S
+            case -1: return self.matrix[:, 0]    # W
+        raise ValueError(f"Direction {d} not defined")
 
-    # get oriented matrix
-    def oriented(self) -> np.ndarray:
-        assert (self.orientation is not None)
-        rot, flip = self.orientation
-        a = np.rot90(self.matrix, rot)
-        return a if flip == 0 else np.fliplr(a)
+    # [north_edge,west_edge]
+    def get_north_and_west_edges(self) -> tuple[Edge, Edge]:
+        return (self.get_edge(DIRS["N"]), self.get_edge(DIRS["W"]))
 
-    # 8 different edges for all symmetries
-    def symmetry_edges(self) -> Iterator[np.ndarray]:
-        a = self.matrix
-        yield (N := a[0, :])  # N
-        yield np.flipud(N)
-        yield (W := a[:, 0])  # W
-        yield np.flipud(W)
-        yield (S := a[-1, :])  # S
-        yield np.flipud(S)
-        yield (E := a[:, -1])  # E
-        yield np.flipud(E)
-
-    def __repr__(self) -> str:
-        return str(self.idx)
+    # all 8 different edges with respect to rotations/flips
+    def all_symmetry_edges(self) -> Iterator[Edge]:
+        for d in ((0, ...), (..., 0), (-1, ...), (..., -1)):
+            edge = self.matrix[d]
+            yield edge
+            yield np.flipud(edge)
 
 
-def init_image(data: str) -> Image:
+def create_tiles(data: str) -> list[Tile]:
     tiles = []
     for tile_str in data.split("\n\n"):
         tile_str = tile_str.splitlines()
@@ -73,100 +59,116 @@ def init_image(data: str) -> Image:
         matrix = np.array([[1 if c == "#" else 0 for c in row] for row in matrix_str])
         tile = Tile(int(idx[:-1]), matrix)
         tiles.append(tile)
-    return Image(tiles)
+    return tiles
 
 
-def get_edge(matrix: np.ndarray, d: Dir) -> np.ndarray:
-    match d:
-        case -1j: return matrix[0, :]   # N
-        case   1: return matrix[:, -1]  # E
-        case  1j: return matrix[-1, :]  # S
-        case -1: return matrix[:, 0]    # W
-    raise ValueError(f"Direction {d} not defined")
-
-
-def get_wanted_edges(grid: Grid, pos: Pos) -> list[np.ndarray | None]:
-    res = []
-    for d in (DIRS["N"], DIRS["W"]):
-        n = pos+d
-        if n in grid and grid[n] is not None:
-            res.append(get_edge(grid[n].oriented(), -d))
-        else:
-            res.append(None)
-    return res
-
-
-def solve(image: Image) -> None:
+def create_grid(tiles: list[Tile]) -> tuple[Grid, int]:
     # collect all possible edges
-    edges: defaultdict[tuple[int], list[Tile]] = defaultdict(list)
-    for tile in image.tiles:
-        for edge in tile.symmetry_edges():
-            edges[tuple(edge)].append(tile)
+    edges_to_tiles: defaultdict[EdgeTuple, set[Tile]] = defaultdict(set)
+    for tile in tiles:
+        for edge in tile.all_symmetry_edges():
+            edges_to_tiles[tuple(edge)].add(tile)
 
     # find edge and corner tiles
     # "..., but the outermost edges won't line up with any other tiles"
-    edge_tiles: Counter[Tile] = Counter(tiles[0] for edge, tiles in edges.items() if len(tiles) == 1)
+    edge_tiles: Counter[Tile] = Counter(tiles.pop() for tiles in edges_to_tiles.values() if len(tiles) == 1)
     corner_tiles: list[Tile] = [tile for tile, count in edge_tiles.items() if count == 4]
     assert len(corner_tiles) == 4, f"Found {len(corner_tiles)} corner tiles, expected 4."
-    print("Part 1:", prod(tile.idx for tile in corner_tiles))
+    p1 = prod(tile.idx for tile in corner_tiles)
 
     # init grid for stitching, y downwards
-    dim = image.dim
+    dim = int(np.sqrt(len(tiles)))
+    assert dim*dim == len(tiles), f"No quadratic shape possible for {len(tiles)} tiles."
     grid: dict[Pos, Tile | None] = {x+y*1j: None for x in range(dim) for y in range(dim)}
 
     # pick any corner tile as the top left corner
-    grid[0] = corner_tiles[0]
+    top_left_corner = corner_tiles[0]
+    grid[0] = top_left_corner
 
     # find correct orientation for the top left corner
-    for symmetry, orientation in grid[0].symmetries():
-        north_edge = get_edge(symmetry, DIRS["N"])
-        west_edge = get_edge(symmetry, DIRS["W"])
-        if len(edges[tuple(north_edge)]) == 1 == len(edges[tuple(west_edge)]):
+    for _ in top_left_corner.transform():
+        north_and_west_edges = top_left_corner.get_north_and_west_edges()
+        if all(len(edges_to_tiles[tuple(e)]) == 0 for e in north_and_west_edges):  # got popped above
             break
     else:
         raise ValueError("Aligning top left corner failed.")
-    grid[0].orientation = orientation
 
     # iterate grid
+    # y-axis first, then x-axis
     for pos in grid:
 
         # top left is done
         if pos == 0:
             continue
 
-        # must share edge with north/west neighbor
+        # must share edge with north and/or west neighbor
         # those neighbors are already correcly orientated
-        shared_edges = []
-        neighbors = set()
+        shared_edges: list[tuple[Edge, Dir]] = []
+        neighbors: set[Tile] = set()
         for d in (DIRS["N"], DIRS["W"]):
             if (neighbor := grid.get(pos+d, None)):
-                shared_edge = get_edge(neighbor.oriented(), -d)
-                shared_edges.append(tuple(shared_edge))
+                shared_edge = neighbor.get_edge(-d)  # -d cuz mirrored
+                shared_edges.append((shared_edge, d))
                 neighbors.add(neighbor)
 
-        candidates = set()
-        for edge in shared_edges:
-            candidates.update(edges[edge])
+        candidates: set[Tile] = set()
+        for edge, _ in shared_edges:
+            candidates.update(edges_to_tiles[tuple(edge)])
         candidates.difference_update(neighbors)
 
         # only one candidate should be left
         assert len(candidates) == 1, f"No Tile found for position {pos}"
-        tile: Tile = list(candidates)[0]
+        tile = candidates.pop()
 
-        # find correct orientation
-        wanted_edges = get_wanted_edges(grid, pos)
-        # print(wanted_edges)
-
-        for transformed_edges, orientation in tile.north_and_west_edges():
-            if all((a is None) or (b is None) or (a == b).all() for a, b in zip(wanted_edges, transformed_edges)):
+        # find correct transformation
+        for _ in tile.transform():
+            if all((shared_edge == tile.get_edge(d)).all() for shared_edge, d in shared_edges):
                 break
         else:
             raise ValueError(f"Aligning tile {grid[pos]} in position {pos} failed.")
 
-        tile.orientation = orientation
         grid[pos] = tile
 
-    print(grid)
+    return grid, p1
+
+
+def create_image(grid: Grid) -> Tile:
+    dim_grid = round(np.sqrt(len(grid)))
+    dim_tile = grid[0].matrix.shape[0] - 2  # type: ignore
+    dim_image = dim_grid * dim_tile
+    image = np.zeros((dim_image, dim_image), dtype=int)
+
+    for x, y in product(range(dim_grid), repeat=2):
+        pos = x + y*1j
+        image[y*dim_tile: (y+1)*dim_tile,
+              x*dim_tile: (x+1)*dim_tile] = grid[pos].matrix[1:-1, 1:-1]  # type:ignore
+
+    return Tile(0, image)
+
+
+def find_sea_monsters(image: Tile) -> int:
+    monster = np.array([
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1],
+        [0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0]
+    ])
+    pixel_monster = monster.sum()
+    pixel_image = image.matrix.sum()
+    for _ in image.transform():
+        corr = convolve(image.matrix, monster, mode="constant")
+        if (mask_monsters := (corr == pixel_monster)).any():
+            break
+    else:
+        raise ValueError("Could not detect any monster.")
+
+    image.matrix[mask_monsters] = 5
+    return pixel_image - mask_monsters.sum() * pixel_monster
+
+
+def plot_tile(tile: Tile) -> None:
+    import matplotlib.pyplot as plt
+    plt.imshow(tile.matrix)
+    plt.show()
 
 
 s = timer()
@@ -176,8 +178,16 @@ input_path = os.path.join(dir_path, "input.txt")
 with open(input_path) as f:
     data = f.read()
 
-image = init_image(data)
-solve(image)
+tiles = create_tiles(data)
+grid, p1 = create_grid(tiles)
+print("Part 1:", p1)
+
+image = create_image(grid)
+p2 = find_sea_monsters(image)
+print("Part 2:", p2)
+
 
 e = timer()
 print("time:", e - s)
+
+plot_tile(image)
